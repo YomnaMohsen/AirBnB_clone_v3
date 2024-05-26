29b8956a1c31ce5a823a9f6f07e4665f86382ae2
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-26 22:15:37 +0300

    id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,10 +6,16 @@ from models.state import State
 from models import storage
 
 
-@app_views.route('/states', methods=['GET'])
-def state():
-    state_list = []
-    state_dict = storage.all(State)
-    for val in state_dict.values():
-        state_list.append(val.to_dict())
-    return jsonify(state_list)
+@app_views.route('/states', methods=['GET', 'POST', 'PUT', "DELETE"])
+@app_views.route('/states/<state_id>', methods=['GET'])
+def state(state_id=None):
+    if request.method == 'GET':
+        if state_id is None:
+            state_list = []
+            state_dict = storage.all(State)
+            for val in state_dict.values():
+                state_list.append(val.to_dict())
+            return jsonify(state_list)
+        else:
+            obj = storage.get(State, state_id)
+            return jsonify(obj.to_dict())
