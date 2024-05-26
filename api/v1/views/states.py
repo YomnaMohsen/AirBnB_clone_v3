3c275cff51b5b9f6110164539e423992385d73d9
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-26 23:44:35 +0300

    sep logic

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,29 +6,15 @@ from models.state import State
 from models import storage
 
 
-@app_views.route('/states', methods=['GET', 'DELETE','POST', 'PUT', ])
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'POST', 'PUT'])
-def state(state_id=None):
+@app_views.route('/states', methods=['GET','POST'])
+def state():
     if request.method == 'GET':
-        if state_id is None:
-            state_list = []
-            state_dict = storage.all(State)
-            for val in state_dict.values():
-                state_list.append(val.to_dict())
-            return jsonify(state_list)
-        else:
-            obj = storage.get(State, state_id)
-            if not obj:
-                abort(404)
-            return jsonify(obj.to_dict())
-    if request.method == 'DELETE':
-        obj = storage.get(State, state_id)
-        if not obj:
-            abort(404)
-        storage.delete(obj)
-        storage.save()
-        return jsonify({})
-    
+        state_list = []
+        state_dict = storage.all(State)
+        for val in state_dict.values():
+            state_list.append(val.to_dict())
+        return jsonify(state_list)
+       
     if request.method == 'POST':
         req_data = request.get_json()
         if req_data is None:
@@ -37,4 +23,21 @@ def state(state_id=None):
              abort(400, "Missing name")
         new_state = State(**req_data)
         new_state.save()
-        return jsonify(new_state.to_dict(), 201)            
+        return jsonify(new_state.to_dict(), 201) 
+    
+    
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+def state_id(state_id): 
+     if request.method == 'GET':
+         obj = storage.get(State, state_id)
+         if not obj:
+            abort(404)
+         return jsonify(obj.to_dict())
+     if request.method == 'DELETE':
+        obj = storage.get(State, state_id)
+        if not obj:
+            abort(404)
+        storage.delete(obj)
+        storage.save()
+        return jsonify({})
+              
