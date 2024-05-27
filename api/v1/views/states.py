07c7373a246f3563b33789f6ae28c555c28dd59f
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-27 18:48:21 +0300

    pycode

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,8 +14,8 @@ def state_get():
     for val in state_dict.values():
         state_list.append(val.to_dict())
     return jsonify(state_list)
-       
-    
+
+
 @app_views.route('/states', methods=['POST'])
 def state_post():
     """post new state"""
@@ -28,10 +28,10 @@ def state_post():
     storage.new(new_state)
     storage.save()
     return jsonify(new_state.to_dict()), 201
-    
-    
+
+
 @app_views.route('/states/<state_id>', methods=['GET'])
-def state_getid(state_id): 
+def state_getid(state_id):
     """get state by id"""
     obj = storage.get(State, state_id)
     if not obj:
@@ -41,19 +41,19 @@ def state_getid(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def state_del(state_id):
-    """delete state by id""" 
+    """delete state by id"""
     obj = storage.get(State, state_id)
     if not obj:
         abort(404)
     storage.delete(obj)
     storage.save()
-    return jsonify({}), 200    
+    return jsonify({}), 200
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def state_put(state_id):
-    """update state selected by id""" 
-    ignore_list =["id", "updated_at", "created_at"]
+    """update state selected by id"""
+    ignore_list = ["id", "updated_at", "created_at"]
     obj = storage.get(State, state_id)
     if not obj:
         abort(404)
@@ -64,7 +64,6 @@ def state_put(state_id):
         k: v for k, v in req_data.items() if k not in ignore_list
     }
     for key, val in update_dict.items():
-            setattr(obj, key, val)
-    storage.save()        
-    return jsonify(obj.to_dict()), 200  
-
+        setattr(obj, key, val)
+    storage.save()
+    return jsonify(obj.to_dict()), 200
