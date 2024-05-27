b39f9301a5e911de81707028e0701213f4c6d8fc
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-27 19:23:53 +0300

    amentiy

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Handle states rest apis"""
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models.amenity import Amenity
+from models import storage
+
+
+@app_views.route('/amenities', methods=['GET'])
+def ament_get():
+    """get method to ret amenties"""
+    am_list = []
+    am_dict = storage.all(Amenity)
+    for val in am_dict.values():
+        am_list.append(val.to_dict())
+    return jsonify(am_list)
+
+
+@app_views.route('/amenities', methods=['POST'])
+def ament_post():
+    """post new amenity"""
+    req_data = request.get_json()
+    if req_data is None:
+        return jsonify({"error": "Not json"}), 400
+        # abort(400, )
+    if req_data.get("name") is None:
+        return jsonify({"error": "Missing name"}), 400
+    new_ament = Amenity(**req_data)
+    storage.new(new_ament)
+    storage.save()
+    return jsonify(new_ament.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def ament_getid(amenity_id):
+    """get amenity by id"""
+    obj = storage.get(Amenity, amenity_id)
+    if not obj:
+        abort(404)
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def amenity_del(amenity_id):
+    """delete amenity by id"""
+    obj = storage.get(Amenity, amenity_id)
+    if not obj:
+        abort(404)
+    storage.delete(obj)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def ament_put(amenity_id):
+    """update amen selected by id"""
+    ignore_list = ["id", "updated_at", "created_at"]
+    obj = storage.get(Amenity, amenity_id)
+    if not obj:
+        abort(404)
+    req_data = request.get_json()
+    if req_data is None:
+        return jsonify({"error": "Not json"}), 400
+    update_dict = {
+        k: v for k, v in req_data.items() if k not in ignore_list
+    }
+    for key, val in update_dict.items():
+        setattr(obj, key, val)
+    storage.save()
+    return jsonify(obj.to_dict()), 200
---
+++ api/v1/views/cities.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Handle states rest apis"""
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models.city import City
+from models.state import State
+from models import storage
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'])
+def state_get():
+    """get method to ret cities by state id"""
+    state_list = []
+    state_dict = storage.all(State)
+    for val in state_dict.values():
+        state_list.append(val.to_dict())
+    return jsonify(state_list)
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'])
+def state_id(state_id):
+    """post new state"""
+    req_data = request.get_json()
+    if req_data is None:
+        return jsonify({"error": "Not json"}), 400
+        # abort(400, )
+    if req_data.get("name") is None:
+        return jsonify({"error": "Missing name"}), 400
+    new_state = State(**req_data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'])
+def state_getid(city_id):
+    """get city by id"""
+    obj = storage.get(City, city_id)
+    if not obj:
+        abort(404)
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'])
+def city_del(city_id):
+    """delete city by id"""
+    obj = storage.get(City, city_id)
+    if not obj:
+        abort(404)
+    storage.delete(obj)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states/cities/<city_id>', methods=['PUT'])
+def city_put(city_id):
+    """update city selected by id"""
+    ignore_list = ["id", "updated_at", "created_at"]
+    obj = storage.get(City, city_id)
+    if not obj:
+        abort(404)
+    req_data = request.get_json()
+    if req_data is None:
+        return jsonify({"error": "Not json"}), 400
+    update_dict = {
+        k: v for k, v in req_data.items() if k not in ignore_list
+    }
+    for key, val in update_dict.items():
+        setattr(obj, key, val)
+    storage.save()
+    return jsonify(obj.to_dict()), 200
